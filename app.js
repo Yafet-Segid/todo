87bfb94e18ac99525956288b189d7049a4c372d6
yafet segid <yafetsegid@yafets-MBP.attlocal.net>
2021-06-19 23:43:55 -0500

    Todo list project for practice

--- app.js
+++ app.js
@@ -4,57 +4,48 @@ let buttons = document.getElementById('button');
 let list = document.getElementById('list');
 
 
+// add button function 
 
+buttons.addEventListener('click', function(){
 
-function addToDo(toDo){
-
-    const item = 
-                `
-                <li id="invitelist" >   
-                <button class="fas fa-check-circle"></button>         
-                <p class="inputs " > ${toDo}</p>
-                <button class="fas fa-trash-alt delete"></button>       
-                </li> 
-                 `;
-
-    const postion  = "beforeend";
-
-    list.insertAdjacentHTML(postion, item);
  
- }
+    // 
+    const todoText = document.createTextNode(input.value);
+    const li = document.createElement('li');
+    li.appendChild(todoText);
+    //
+    const checkButton = document.createElement("button");
+    checkButton.className = "far fa-check-square check";
+    li.appendChild(checkButton);
+    const deletButton = document.createElement("button");
+    deletButton.className = "fas fa-trash-alt delete";
+    li.appendChild(deletButton);
+   
+    list.appendChild(li);
+    input.value = "";
 
- document.addEventListener("keyup",function(even){
-     if(event.keyCode == 13){
-         const toDo = input.value;
-     if(toDo){
-        addToDo(toDo);
-     }
-     input.value = "";
 
+})
+
+  // prevent text from double click
+document.addEventListener('mousedown', function(event){
+    if (event.detail > 1){
+        event.preventDefault();
     }
+}, false);
 
-    buttons.addEventListener('click', function(){
-        addToDo(input.value);
+// delete 
 
-    })
-
-
-    /// remove item 
-    list.addEventListener('click', e =>{
-        if(e.target.classList.contains('delete)')){
-        e.target.addToDo.remove();
-        }
-    })
+list.addEventListener('click', function (e) {
+    if(e.target.classList.contains("delete")) {  
+    e.target.parentElement.remove(); 
+}
+})
 
 
-    //line-through
-
-    // document.addEventListener('click', function(cross){
-    //     if(cross == Check){
-    //         addToDo(toDo.style.textDecoration = "line-through");
-
-    //     }
-    // })
-     
-});
-
+// line- throught 
+list.addEventListener('click', function (e) {
+    if(e.target.classList.contains("check")){
+        e.target.parentElement.style.textDecoration = "line-through";
+    }
+})
